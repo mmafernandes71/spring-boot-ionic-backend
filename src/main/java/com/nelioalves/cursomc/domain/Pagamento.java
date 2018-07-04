34ad7d6a5c86b85836f0087fd4131440ccb4f872
Nelio Alves <mmafernandes71@gmail.com>
2018-07-03 23:13:26 -0300

    Correcoes: update em categoriaservice e construtor de pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -20,6 +20,7 @@ public abstract class Pagamento implements Serializable{
 	
 	@Id
 	private Integer id;
+	
 	private Integer estado;
 	
 	@JsonIgnore
@@ -35,7 +36,7 @@ public abstract class Pagamento implements Serializable{
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
 		super();
 		this.id = id;
-		this.estado = estado.getCod();
+		this.estado = (estado==null) ? null : estado.getCod();
 		this.pedido = pedido;
 	}
 
