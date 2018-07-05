f7c7ef2ab9e39efd17d741a209d8e127ad031ec0
Nelio Alves <mmafernandes71@gmail.com>
2018-07-04 22:57:43 -0300

    Validacao customizada: email nao repetido na insercao de cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
@@ -2,15 +2,15 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Email; 
-import javax.validation.constraints.NotEmpty;
-
 import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
