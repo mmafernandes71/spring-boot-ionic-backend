9bc6a20127f1ff3fd7822a2734478f872bac554c
Nelio Alves <mmafernandes71@gmail.com>
2018-07-12 13:16:00 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,24 @@
 package com.nelioalves.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.security.UserSS;
+import com.nelioalves.cursomc.services.UserService;
+import com.nelioalves.cursomc.services.exceptions.AuthorizationException;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
+
 
 }
--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -6,16 +6,21 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.ItemPedido;
 import com.nelioalves.cursomc.domain.PagamentoComBoleto;
 import com.nelioalves.cursomc.domain.Pedido;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
-import com.nelioalves.cursomc.repositories.ClienteRepository;
 import com.nelioalves.cursomc.repositories.ItemPedidoRepository;
 import com.nelioalves.cursomc.repositories.PagamentoRepository;
 import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.security.UserSS;
+import com.nelioalves.cursomc.services.exceptions.AuthorizationException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -77,6 +82,16 @@ public class PedidoService {
 		return obj;
 		
 	}	
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 	
 
 }
